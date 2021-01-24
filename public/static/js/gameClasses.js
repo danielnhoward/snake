0da1012c5214f2d9a7a9b766db51fb05dc46144e
Toffee1347 <danielnhoward93@gmail.com>
2021-01-24 21:17:16 +0000

    added color gradient on snake

--- public/static/js/gameClasses.js
+++ public/static/js/gameClasses.js
@@ -141,10 +141,10 @@ class Snake {
         });
     };
     drawSnake() {
-        this.ctx.fillStyle = this.settings.snakeColour;
-        this.ctx.strokeStyle = this.settings.snakeBorderColour;
-        this.snake.forEach((part) => {
-            this.ctx.fillRect(part.x, part.y, this.blockSize, this.blockSize);
+        this.snake.forEach((part, index) => {
+            this.ctx.fillStyle = shadeColor(this.settings.snakeColour, -(50 / (this.snake.length - 1)) * index);
+            this.ctx.strokeStyle = shadeColor(this.settings.snakeBorderColour, -(50 / (this.snake.length - 1)) * index);
+            this.ctx.fillRect(part.x, part.y, this.blockSize + 1, this.blockSize);
             this.ctx.strokeRect(part.x, part.y, this.blockSize, this.blockSize);
         });
     };
@@ -205,7 +205,7 @@ class Snake {
     addScore(amount) {
         this.score += amount;
         document.getElementById(this.scoreId).innerHTML = this.score;
-    }
+    };
     init() {
         this.ready = true;
         for (let x = 1; x < this.startLength; x++) {
@@ -247,7 +247,7 @@ class Snake {
                 });
             };
         });
-    }
+    };
     restart() {
         if (this.intervalRunning) {
             clearInterval(this.interval);
@@ -259,12 +259,32 @@ class Snake {
         this.velocety = {x:0, y:0};
         this.score = 0;
         this.init();
-    }
+    };
     setState(state) {
         this.running = state;
-    }
-}
+    };
+};
 
 function createTime(minutes, seconds) {
     return `${`0${minutes}`.slice(-2)}:${`0${seconds}`.slice(-2)}`;
-}
+};
+
+function shadeColor(color, percent) {
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;  
+    B = (B<255)?B:255;  
+
+    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
+};
