f933bfee017436ece416214091df979af412ef29
Toffee1347 <danielnhoward93@gmail.com>
2021-01-28 13:16:20 +0000

    multi death

--- gameClass.js
+++ gameClass.js
@@ -19,7 +19,7 @@ module.exports = class {
             emit: emit,
             name: name,
             settings: settings,
-            snake: new Snake(settings, this.canvas)
+            snake: new Snake(settings, this.canvas, () => {this.playerDie(id)})
         });
         this.playerIds.push = id;
         this.playerCount = this.players.length;
@@ -35,12 +35,28 @@ module.exports = class {
                 this.emit('playerCount', this.playerCount);
             };
         });
+        if (this.players.length == 0) {
+            clearInterval(this.interval);
+            delete this;
+        };
+    };
+    playerDie(id) {
+        this.players.forEach((player, index) => {
+            if (player.id == id) {
+                player.emit('snakeDeath');
+                this.players = this.players.filter((value, filterIndex) => {
+                    return filterIndex != index;
+                });
+                this.playerCount = this.players.length;
+                this.emit('playerCount', this.playerCount);
+            };
+        });
     };
     run() {
         this.interval = setInterval(() => {
             this.canvas = [];
             for (const player of this.players) {
-                this.canvas = [...this.canvas, ...player.snake.getSnake()];
+                this.canvas = [...this.canvas, ...player.snake.moveSnake()];
             }
             this.emit('snakePing', this.canvas);
         }, this.gameSpeed);
@@ -48,8 +64,10 @@ module.exports = class {
 };
 
 class Snake {
-    constructor(settings, initCanvas) {
+    constructor(settings, initCanvas, onDie) {
         this.settings = settings;
+        this.onDie = onDie;
+        this.velocety = {x: 40, y: 0};
         this.initSnake(initCanvas);
     };
     initSnake(initCanvas) {
@@ -65,7 +83,27 @@ class Snake {
             };
         };
     };
-    getSnake() {
+    moveSnake() {
+        if (this.gameEnded()) {
+            this.onDie();
+            return [];
+        };
+        this.snake.unshift({x:this.snake[0].x + this.velocety.x, y:this.snake[0].y + this.velocety.y, colour: {border: this.settings.snakeBorderColour, body: this.settings.snakeColour}});
+        this.snake.pop();
         return this.snake;
+    };
+    gameEnded() {
+        for (let i = 1; i < this.snake.length; i++) {
+            if (this.snake[i].x === this.snake[0].x && this.snake[i].y === this.snake[0].y) {
+                return true;
+            };
+        };
+        const hits = {
+            leftWall: this.snake[0].x < 0,
+            rightWall: this.snake[0].x > 960,
+            topWall: this.snake[0].y < 0,
+            bottomWall: this.snake[0].y > 960
+        };
+        return hits.leftWall || hits.rightWall || hits.topWall || hits.bottomWall;
     };
 }
